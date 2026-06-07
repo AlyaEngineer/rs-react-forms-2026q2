0881fa6cb4439079aeb0c959860c747c25f59651
Alla Tsaiukova <front.dev.edu@gmail.com>
2026-06-08 00:03:54 +0200

    refactor(password-field): move password strength indicator below error message

--- src/components/UncontrolledForm/fields/PasswordField.tsx
+++ src/components/UncontrolledForm/fields/PasswordField.tsx
@@ -6,18 +6,20 @@ const PasswordField = ({ error }: { error?: string }) => {
   const [password, setPassword] = useState('');
 
   return (
-    <FormField id="password" label="Password" error={error}>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-      />
+    <>
+      <FormField id="password" label="Password" error={error}>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+        />
+      </FormField>
       <PasswordStrengthIndicator password={password} />
-    </FormField>
+    </>
   );
 };
 
