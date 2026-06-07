ca22c886b78c4945de858a37961e6abf81b758f5
Alla Tsaiukova <front.dev.edu@gmail.com>
2026-06-07 23:58:48 +0200

    refactor(age-field): remove min value constraint from age input

--- src/components/UncontrolledForm/fields/AgeField.tsx
+++ src/components/UncontrolledForm/fields/AgeField.tsx
@@ -6,7 +6,6 @@ const AgeField = ({ error }: { error?: string }) => (
       id="age"
       name="age"
       type="number"
-      min="0"
       className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
     />
   </FormField>
